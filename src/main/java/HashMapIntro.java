c2dcd84e9e1d6b169bef362424aa0410ab532989
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-26 16:06:34 +0000

    Added hashmap and comments of a few of its methods

--- src/main/java/HashMapIntro.java
+++ src/main/java/HashMapIntro.java
@@ -1,2 +1,24 @@
+import java.util.HashMap;
+
 public class HashMapIntro {
+
+    public static void main(String[] args) {
+        HashMap<String, String> faveFruits = new HashMap<String, String>();
+
+        faveFruits.put("Nawaz", "Amb");
+        faveFruits.put("Me", "Amroot");
+        faveFruits.put("Them", "Khela");
+
+        String myFave = faveFruits.get("Nawaz");
+        System.out.println(myFave);
+        System.out.println(faveFruits.get("Me"));
+
+        // put(key, value: Insert a new key/value pair into the hashmap
+        // get(jey): Returns the value for the given key
+        // size(): Returns the number of key/value pairs in the hashmap
+        // clear(): removes all the key/value pairs in the hashmap.
+        // contains(value): Return boolean if value exists in hashmap.
+
+    }
+
 }
